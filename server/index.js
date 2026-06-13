c1d252227dac5dd439dd1625833b091c2d592acb
Sumit Kumar <sumit76231@gmail.com>
2026-06-13 18:18:01 +0530

    feat: enable Swagger/OpenAPI docs at /api-docs

--- server/index.js
+++ server/index.js
@@ -19,7 +19,8 @@ import http from "http";
 import { Server } from "socket.io";
 import { logEvaluatorConfig } from "./src/config/evaluatorConfig.js";
 import redisClient, { connectRedis } from "./src/config/redis.js";
-// import swaggerSpec from "./src/config/swaggerConfig.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./src/config/swaggerConfig.js";
 import connectDB, { isConnected } from "./src/database/db.js";
 import { protect, verifySocketToken } from "./src/middleware/authMiddleware.js";
 import globalErrorHandler from "./src/middleware/errorMiddleware.js";
@@ -195,7 +196,7 @@ app.get("/health", (req, res) => {
   });
 });
 
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 
